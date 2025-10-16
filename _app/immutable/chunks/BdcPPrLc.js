2873d0bb027f97d15574196cc0dd84b4ad3459fd
aqumu <aqumu.vm@gmail.com>
2025-10-16 18:49:29 +0700

    Deploy to GitHub Pages

---
+++ _app/immutable/chunks/BdcPPrLc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_mecphu?.base??"/vmboards-design",e=globalThis.__sveltekit_mecphu?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/op4GsI39.js
@@ -0,0 +1,1 @@
+import{x as a}from"./C-oxlGfV.js";a();
